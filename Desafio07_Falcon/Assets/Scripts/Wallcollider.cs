2a46eb3109e0b87c2c772a34a918a8d3df1034a4
MarcosFalcon343 <marcosfalcon2003@gmail.com>
2022-08-11 22:51:04 -0500

    Update Desafio07

--- Desafio07_Falcon/Assets/Scripts/Wallcollider.cs
+++ Desafio07_Falcon/Assets/Scripts/Wallcollider.cs
@@ -4,15 +4,23 @@ using UnityEngine;
 
 public class Wallcollider : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public float cooldown = 2f;
+    public float time = 0;
+    private Transform newposition;
+
+
+    private void OnCollisionEnter(Collision collision)
     {
-        
+        time = 0;
+    }
+    private void OnCollisionStay(Collision other)
+    {
+        time += Time.deltaTime;
+        if (time >= cooldown)
+        {
+            //newposition = new Vector3(Random.Range(-4.0f, 4.0f), 0, Random.Range(-4.0f, 4.0f));
+            other.transform.position = new Vector3(Random.Range(-4.0f, 4.0f), 2, Random.Range(-4.0f, 4.0f));
+        }
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
